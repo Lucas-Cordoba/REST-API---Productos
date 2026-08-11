183b1b46708743b1ad5846b374ffe47e86ce4b9f
Lucas-Cordoba <cssmcordoba@gmail.com>
2026-08-11 16:20:03 -0300

    'Agregamos el frontend, hicimos peticiones a la base de datos, agregamos cors y morgan'

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import colors from "colors";
+import cors, {CorsOptions} from 'cors'
+import morgan from 'morgan'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, {swaggerUiOptions} from "./config/swagger";
 import router from "./router";
@@ -22,8 +24,36 @@ connectDB() //llamamos a la funcion para conectarnos a la base de datos
 //Instancia de express
 const server = express(); //sobre este server se van a crear las rutas y los middlewares
 
+
+//Permitir conexiones
+const corsOptions : CorsOptions = {
+    origin: function (origin, callback ) { //origin es quien lo envia, callback es el que me permite o niega la conexion
+
+        if(origin === process.env.FRONTEND_URL){
+            callback(null, true) //primer parametro el error si es que hay y segundo si permite o no
+        }else{
+
+             callback(new Error('Error de CORS'))
+        }
+
+
+
+    } //el origen es lo que me esta enviando la peticion, es quien me esta enviando la peticion
+}
+server.use(cors(corsOptions))
+
 //Leer datos de formularios
 server.use(express.json()) //con esto le decimos a express que vamos a recibir datos en formato json
+
+//TIPOS DE OPCIONES DE MORGAN
+server.use(morgan('dev')) //estas opciones nos dan un detalle de las peticiones, dev me da status, segundos que tardee
+// server.use(morgan('combined')) //combined me da algo mas detallado, nos viene el tipo y a que url fue, status, navegador, etc
+// server.use(morgan('common')) //Hora, tipo de peticion, url a la que fue, etc
+// server.use(morgan('short')) //status, tipo de peticion, y url, etc
+// server.use(morgan('tiny')) //tipo de peticion, url, status
+
+
+
 server.use('/api/products', router)//nmo es muy flexible, es para decirle a nuestro server que use el router que creamos en router.ts para manejar las rutas de nuestra aplicacion
 
 //DOCS
