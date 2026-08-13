41810648271c18b5e454c2a009b19eb851144949
Lucas-Cordoba <cssmcordoba@gmail.com>
2026-08-13 13:35:10 -0300

    'configuras Sequelize con dialectOptions especificando la configuración de SSL'

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,7 +8,12 @@ const db = new Sequelize(process.env.DATABASE_URL!,{
   models: [__dirname + '/../models/*.model.ts'], //esto es para decirle a sequelize que busque los modelos en la carpeta models, y que los cargue automaticamente, para poder usarlos en nuestra aplicacion
   //dirname es una variable que nos da la ruta absoluta de la carpeta donde estamos, y con eso le decimos a sequelize que busque los modelos en la carpeta models, y que los cargue automaticamente
   logging: false, //esto es para decirle a sequelize que no nos muestre los logs de las consultas que hace a la base de datos
-  
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <--- Esto es clave para aceptar el certificado en Render/Cloud
+    }
+  }
 }//,{
     // dialectOptions:{ //son las opciones que le pasamos a la base de datos para poder conectarnos a ella
     //     ssl: {
